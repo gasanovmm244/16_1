14a0f0f0aca5585dbc2c3c2bec8307fb29be0cb6
gasanovmm244 <gasanovmm244@gmail.com>
2024-11-15 00:26:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ app = FastAPI()
 
 
 @app.get('/user/admin')
-async def welcom():
+async def welcome():
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
-async def welcom(user_id: int):
+async def welcome(user_id: int):
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user')
-async def welcom(username: str, age: int):
+async def welcome(username: str, age: int):
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
 
 
 @app.get('/')
-async def welcom():
+async def welcome():
     return {'message': 'Главная страница'}
